27195c134ae222231688db68095742e65b096fa1
k12club <k12club@hotmail.com>
2018-03-26 23:57:30 +0700

    login

--- Angular5_CLI_Full_Project/src/app/page/login/login.component.ts
+++ Angular5_CLI_Full_Project/src/app/page/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'app/page/model/user';
+import { AngularFireAuth } from 'angularfire2/auth';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -8,17 +10,29 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
+  user:User = { email:'email',password:'password'}
   constructor(
     private route: ActivatedRoute,
     private router: Router,
+    private angularFireAuth:AngularFireAuth,
   ) { }
 
   ngOnInit() {
   }
 
+  signin(user:User){
+    this.angularFireAuth.auth.signInWithEmailAndPassword(user.email,user.password).then(res=>{
+      console.log(res);
+      this.router.navigate(['']);
+      
+    });
+  }
+
   gotoregister(){
     this.router.navigate(['register'])
 
+    
+  
   }
 
 }
